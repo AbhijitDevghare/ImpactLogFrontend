136cca26514518f38ad7eb26268c63576b27839e
AbhijitDevghare <kishordevghare6@gmail.com>
2025-12-06 17:13:04 +0530

    socket url chaneged

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 
 export const useSocket = () => {
   
-  const api = import.meta.env.VITE_CHAT_SERVICE_API_URL || 'https://api.impactlog.me/chat';
+  const api = 'https://api.impactlog.me';
 
 
   const dispatch = useDispatch();
