4580d17dc200ab2ad8e5f75448f8f160daa86ccb
AbhijitDevghare <kishordevghare6@gmail.com>
2026-02-18 17:08:54 +0530

    Some changes in completed event page

--- src/pages/CompletedEventsPage.jsx
+++ src/pages/CompletedEventsPage.jsx
@@ -101,7 +101,7 @@ export default function CompletedEventsPage() {
       )}
 
       {/* Events List */}
-      {!loading && !error && completedEvents.length > 0 && (
+      {!loading  && completedEvents.length > 0 && (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {completedEvents.map((event) => (
             <div
